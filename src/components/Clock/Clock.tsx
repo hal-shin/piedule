755d50a4794afb299e981bb200dc3d587035b920
Hal Shin <hal.shin@alumni.ubc.ca>
2023-10-25 13:54:03 -0700

    Clean up imports

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import { type Slice } from '@prisma/client';
 import React, { useCallback, useMemo } from 'react';
 import {
